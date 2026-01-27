ddd7c5af0746036dc64574af2c12e3df90f24005
aluabagdat <alukabagdat@gmail.com>
2026-01-27 17:54:48 +0500

    Create task6.java

---
+++ lab1/task6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        
+        StringBuilder reversed = new StringBuilder(input).reverse();
+        
+        if (input.equals(reversed.toString())) {
+            System.out.println("string is a palindrome");
+        } else {
+            System.out.println("string is NOT a palindrome");
+        }
+        
+        scanner.close();
+    }
+}
