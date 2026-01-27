3e4cf9408613c0ed72a39898fd726795e29ae6c0
aluabagdat <alukabagdat@gmail.com>
2026-01-28 01:14:15 +0500

    Update task1.java

--- lab1/task1.java
+++ lab1/task1.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class PrintMyName {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
+        System.out.print("your name : ");
         String name = scanner.nextLine();
 
         int length = name.length();
-        int width = length + 2;
+        int width = length;
         
         System.out.print("+");
         for (int i = 0; i < width; i++) {
